c84eadcbb27717456209be604484b2a81673eeb1
Rebakure <kevinrebakure@gmail.com>
2024-07-06 20:26:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ function createTask(getTASK: Task) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const deleteButton = document.createElement("button");
-  // const doneButton = document.createElement("button");
+  const doneButton = document.createElement("button");
 
   li.className = "flex items-center justify-end bg-yellow-100 px-3 py-1";
   li.id = `${getTASK.id}`;
@@ -54,10 +54,10 @@ function createTask(getTASK: Task) {
   deleteButton.className = "border bg-red-500 px-3 py-1 text-white";
   deleteButton.addEventListener("click", deleteTask);
 
-  // doneButton.innerText = "Done";
-  // doneButton.id = "done";
-  // doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
-  // doneButton.addEventListener('click', checkTask)
+  doneButton.innerText = "Done";
+  doneButton.id = "done";
+  doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
+  doneButton.addEventListener('click', checkTask)
 
   li.append(span, deleteButton);
   // li.append(span, deleteButton, doneButton);
@@ -82,9 +82,15 @@ function deleteTask(e: any) {
 
 // Check tasks
 
-// function checkTask(e: any) {
-//   console.log('CHECK TASK FUNCTION')
-//   const parent = e.target.parentElement;
-//   const text = parent.querySelector('#task')
-//   console.log(text) 
-// }
+function checkTask(e: any) {
+  console.log("CHECK TASK FUNCTION");
+  console.log("Parent element (Li) id:", e.target.parentElement.id);
+  ul.removeChild(e.target.parentElement);
+  console.log(typeof e.target.parentElement.id);
+  const removeIndex = tasks.findIndex(
+    (task: Task) => task.id === parseInt(e.target.parentElement.id),
+  );
+  tasks.splice(removeIndex, 1);
+  console.log("Remove index:", removeIndex);
+  console.log("All tasks", tasks);
+}

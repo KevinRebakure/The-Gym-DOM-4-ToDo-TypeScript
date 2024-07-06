d52e1c6a1351007fa0b7beb08495f23bec7853e6
Rebakure <kevinrebakure@gmail.com>
2024-07-06 16:42:03 +0200

    scheduled ⌛- work on done, and edit features

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@ const DONE = document.getElementById("done");
 const tasks = [];
 let id = 0;
 form.addEventListener("submit", function (e) {
-    console.log('FORM SUBMIT CALLBACK FUNCTION');
+    console.log("FORM SUBMIT CALLBACK FUNCTION");
     e.preventDefault();
     const TASK = {
         id: id,
@@ -15,18 +15,19 @@ form.addEventListener("submit", function (e) {
         completed: false,
     };
     tasks.push(TASK);
-    console.log('Created task object and pushed it to tasks:', tasks);
+    console.log("Created task object and pushed it to tasks:", tasks);
     createTask(TASK);
     input.value = "";
     id++;
-    console.log('Incremented id:', id);
+    console.log("Incremented id:", id);
 });
+// Create task
 function createTask(getTASK) {
-    console.log('CREATE TASK FUNCTION');
+    console.log("CREATE TASK FUNCTION");
     const li = document.createElement("li");
     const span = document.createElement("span");
     const deleteButton = document.createElement("button");
-    const doneButton = document.createElement("button");
+    // const doneButton = document.createElement("button");
     li.className = "flex items-center justify-end bg-yellow-100 px-3 py-1";
     li.id = `${getTASK.id}`;
     span.innerText = getTASK.task;
@@ -36,15 +37,18 @@ function createTask(getTASK) {
     deleteButton.id = "delete";
     deleteButton.className = "border bg-red-500 px-3 py-1 text-white";
     deleteButton.addEventListener("click", deleteTask);
-    doneButton.innerText = "Done";
-    doneButton.id = "done";
-    doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
-    li.append(span, deleteButton, doneButton);
+    // doneButton.innerText = "Done";
+    // doneButton.id = "done";
+    // doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
+    // doneButton.addEventListener('click', checkTask)
+    li.append(span, deleteButton);
+    // li.append(span, deleteButton, doneButton);
     ul.prepend(li);
-    console.log('Create task function li element:', li);
+    console.log("Create task function li element:", li);
 }
+// Delete task
 function deleteTask(e) {
-    console.log('DELETE TASK FUNCTION');
+    console.log("DELETE TASK FUNCTION");
     console.log("Parent element (Li) id:", e.target.parentElement.id);
     ul.removeChild(e.target.parentElement);
     console.log(typeof e.target.parentElement.id);
@@ -53,3 +57,10 @@ function deleteTask(e) {
     console.log("Remove index:", removeIndex);
     console.log("All tasks", tasks);
 }
+// Check tasks
+// function checkTask(e: any) {
+//   console.log('CHECK TASK FUNCTION')
+//   const parent = e.target.parentElement;
+//   const text = parent.querySelector('#task')
+//   console.log(text) 
+// }
--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ interface Task {
 
 let id = 0;
 form.addEventListener("submit", function (e) {
-    console.log('FORM SUBMIT CALLBACK FUNCTION')
+  console.log("FORM SUBMIT CALLBACK FUNCTION");
   e.preventDefault();
 
   const TASK: Task = {
@@ -25,45 +25,50 @@ form.addEventListener("submit", function (e) {
   };
 
   tasks.push(TASK);
-  console.log('Created task object and pushed it to tasks:', tasks);
+  console.log("Created task object and pushed it to tasks:", tasks);
 
   createTask(TASK);
   input.value = "";
   id++;
-  console.log('Incremented id:', id);
+  console.log("Incremented id:", id);
 });
 
-function createTask(getTASK:Task) {
-    console.log('CREATE TASK FUNCTION')
+// Create task
+
+function createTask(getTASK: Task) {
+  console.log("CREATE TASK FUNCTION");
   const li = document.createElement("li");
   const span = document.createElement("span");
   const deleteButton = document.createElement("button");
-  const doneButton = document.createElement("button");
+  // const doneButton = document.createElement("button");
 
   li.className = "flex items-center justify-end bg-yellow-100 px-3 py-1";
   li.id = `${getTASK.id}`;
 
-  
   span.innerText = getTASK.task;
   span.id = "task";
   span.className = "mr-auto";
-  
+
   deleteButton.innerText = "Delete";
   deleteButton.id = "delete";
   deleteButton.className = "border bg-red-500 px-3 py-1 text-white";
   deleteButton.addEventListener("click", deleteTask);
-  
-  doneButton.innerText = "Done";
-  doneButton.id = "done";
-  doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
-  
-  li.append(span, deleteButton, doneButton);
+
+  // doneButton.innerText = "Done";
+  // doneButton.id = "done";
+  // doneButton.className = "border bg-blue-500 px-3 py-1 text-white";
+  // doneButton.addEventListener('click', checkTask)
+
+  li.append(span, deleteButton);
+  // li.append(span, deleteButton, doneButton);
   ul.prepend(li);
-  console.log('Create task function li element:', li);
+  console.log("Create task function li element:", li);
 }
 
+// Delete task
+
 function deleteTask(e: any) {
-    console.log('DELETE TASK FUNCTION')
+  console.log("DELETE TASK FUNCTION");
   console.log("Parent element (Li) id:", e.target.parentElement.id);
   ul.removeChild(e.target.parentElement);
   console.log(typeof e.target.parentElement.id);
@@ -74,3 +79,12 @@ function deleteTask(e: any) {
   console.log("Remove index:", removeIndex);
   console.log("All tasks", tasks);
 }
+
+// Check tasks
+
+// function checkTask(e: any) {
+//   console.log('CHECK TASK FUNCTION')
+//   const parent = e.target.parentElement;
+//   const text = parent.querySelector('#task')
+//   console.log(text) 
+// }

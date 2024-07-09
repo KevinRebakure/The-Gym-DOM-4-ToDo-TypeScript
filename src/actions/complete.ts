f339c6e81c1b693993359553c1cc92e4b44aa8d5
Rebakure <kevinrebakure@gmail.com>
2024-07-09 14:35:08 +0200

    a

--- src/actions/complete.ts
+++ src/actions/complete.ts
@@ -1,18 +1,21 @@
+import { Task } from "../interfaces";
+
 let clicked = false;
 export default function complete(
+  _tasks: Task[],
   _id: number,
   _checkButton: HTMLButtonElement,
 ) {
-  // const index = _tasks.findIndex((task) => task.id == _id);
   if (!clicked) {
-    _checkButton.innerText = "Completed";
+    // _checkButton.innerText = "Completed";
     // _checkButton.className = _checkButton.className + " line-through";
     clicked = true;
-    _checkButton.parentElement!.firstElementChild!.className = "mr-auto line-through";
+    // _checkButton.parentElement!.firstElementChild!.className =
+    //   "mr-auto line-through";
   } else {
-    _checkButton.innerText = "Check";
+    // _checkButton.innerText = "Check";
     // _checkButton.className = "border bg-blue-500 px-3 py-1 text-white";
     clicked = false;
-    _checkButton.parentElement!.firstElementChild!.className = "mr-auto";
+    // _checkButton.parentElement!.firstElementChild!.className = "mr-auto";
   }
 }
--- src/actions/populateUI.ts
+++ src/actions/populateUI.ts
@@ -3,42 +3,49 @@ import complete from "./complete";
 import edit from "./edit";
 import remove from "./remove";
 
-export default function populateUI(_ul:HTMLUListElement, _tasks:Task[]) {
-    _ul.innerHTML = ''
-    _tasks.map((task)=>{
-        const li = document.createElement("li");
-        li.id = task.id.toString() 
-        li.className = "flex items-center justify-end bg-yellow-100 px-3 py-1";
+export default function populateUI(_ul: HTMLUListElement, _tasks: Task[]) {
+  _ul.innerHTML = "";
+  _tasks.map((task) => {
+    const li = document.createElement("li");
+    li.id = task.id.toString();
+    li.className = "flex items-center justify-end bg-yellow-100 px-3 py-1";
 
-        const span = document.createElement("span");
-        span.className = "mr-auto";
-        span.innerText = task.task;
+    const span = document.createElement("span");
+    span.className = "mr-auto";
+    span.innerText = task.task;
 
-        const editButton = document.createElement("button");
-        editButton.className = "border bg-green-500 px-3 py-1 text-white";
-        editButton.innerText = "Edit"
-        editButton.addEventListener('click', function(){
-            edit(_tasks, task.id, editButton)
-            // populateUI(_ul, _tasks)
-        })
+    const editButton = document.createElement("button");
+    editButton.className = "border bg-green-500 px-3 py-1 text-white";
+    editButton.innerText = "Edit";
+    editButton.addEventListener("click", function () {
+      edit(_tasks, task.id, editButton);
+      // populateUI(_ul, _tasks)
+    });
 
-        const checkButton = document.createElement("button");
-        checkButton.className = "border bg-blue-500 px-3 py-1 text-white";
-        checkButton.innerText = "Check"
-        checkButton.addEventListener('click', function() {
-            complete(task.id, checkButton)
-        })
+    const checkButton = document.createElement("button");
+    checkButton.className = "border bg-blue-500 px-3 py-1 text-white";
+    if (!task.completed) {
+      checkButton.innerText = "Check";
+      span.className = "mr-auto";
+    } else {
+      checkButton.innerText = "Completed";
+      span.className = "mr-auto line-through";
+    }
 
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "border bg-red-500 px-3 py-1 text-white";
-        deleteButton.innerText = "Delete"
-        deleteButton.addEventListener("click", function() {
-             remove(_tasks, task.id),
-             populateUI(_ul, _tasks)
-        });
+    checkButton.addEventListener("click", function () {
+      complete(_tasks, task.id, checkButton);
+      populateUI(_ul, _tasks);
+    });
 
-        // li.append(span, editButton, deleteButton);
-        li.append(span, editButton, checkButton, deleteButton);
-        _ul.append(li)
-    })
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "border bg-red-500 px-3 py-1 text-white";
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", function () {
+      remove(_tasks, task.id), populateUI(_ul, _tasks);
+    });
+
+    // li.append(span, editButton, deleteButton);
+    li.append(span, editButton, checkButton, deleteButton);
+    _ul.append(li);
+  });
 }
